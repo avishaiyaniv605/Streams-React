9ecc5229aefb029589913e121d48bb9233aa6371
avishaiyaniv <avishaiy@post.bgu.il>
2019-09-05 20:50:34 +0300

    stream_create is done

---
+++ client/src/components/streams/StreamCreate.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    return (
+      <input { ...formProps.input } />
+    );
+  }
+
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+export default reduxForm({
+  form: "NEW_STREAM"
+})(StreamCreate);
